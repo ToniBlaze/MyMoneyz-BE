0457b8d77b45793fea8097a256357853e0f8a6bd
Alessio <alessio.toninello@gmail.com>
2024-10-28 11:52:34 +0100

    format

--- endpoints/users.js
+++ endpoints/users.js
@@ -1,60 +1,61 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const userModel = require("../models/User");
 
-
-router.get('/users', async (req, res, next) => {
-    try {
-        const users = await userModel.find();
-        res.status(200).json(users);
-    } catch (err) {
-        next(err);
-    }
+router.get("/users", async (req, res, next) => {
+  try {
+    const users = await userModel.find();
+    res.status(200).json(users);
+  } catch (err) {
+    next(err);
+  }
 });
 
-
-router.get('/users/:id', async (req, res, next) => {
-    try {
-        const user = await userModel.findById(req.params.id);
-        if (!user) return res.status(404).json({ error: "Utente non trovato" });
-        res.status(200).json(user);
-    } catch (err) {
-        next(err);
-    }
+router.get("/users/:id", async (req, res, next) => {
+  try {
+    const user = await userModel.findById(req.params.id);
+    if (!user) return res.status(404).json({ error: "Utente non trovato" });
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
 });
 
-
-router.post('/users', async (req, res, next) => {
-    try {
-        const newUser = new userModel(req.body);
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
-    } catch (err) {
-        next(err);
-    }
+router.post("/users", async (req, res, next) => {
+  try {
+    const newUser = new userModel(req.body);
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    next(err);
+  }
 });
 
-
-router.put('/users/:id', async (req, res, next) => {
-    try {
-        const updatedUser = await userModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!updatedUser) return res.status(404).json({ error: "Utente non trovato" });
-        res.status(200).json(updatedUser);
-    } catch (err) {
-        next(err);
-    }
+router.put("/users/:id", async (req, res, next) => {
+  try {
+    const updatedUser = await userModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!updatedUser)
+      return res.status(404).json({ error: "Utente non trovato" });
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    next(err);
+  }
 });
 
-
-router.delete('/users/:id', async (req, res, next) => {
-    try {
-        const deletedUser = await userModel.findByIdAndDelete(req.params.id);
-        if (!deletedUser) return res.status(404).json({ error: "Utente non trovato" });
-        res.status(200).json({ message: "Utente eliminato con successo" });
-    } catch (err) {
-        next(err);
-    }
+router.delete("/users/:id", async (req, res, next) => {
+  try {
+    const deletedUser = await userModel.findByIdAndDelete(req.params.id);
+    if (!deletedUser)
+      return res.status(404).json({ error: "Utente non trovato" });
+    res.status(200).json({ message: "Utente eliminato con successo" });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
